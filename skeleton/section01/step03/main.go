25fcd81d672b271110b49bc5078d312637aae92d
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-09-16 09:19:40 +0900

    spec04まで終了

--- skeleton/section01/step03/main.go
+++ skeleton/section01/step03/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	// TODO: fmtパッケージをインポートする
 
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -16,7 +17,7 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	// TODO: 0から9までの間で乱数を作り変数numに代入する
-
+	num := rand.Intn(10)
 	// 変数numを表示する
 	fmt.Println(num)
 }
